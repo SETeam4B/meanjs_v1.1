404fbe89cf2a224df13be0a7453df967f8d41bf6
Xiaoming Lu <xiaoming@ufl.edu>
2017-10-29 16:51:18 -0400

    modified course model

--- modules/courses/server/models/course.server.model.js
+++ modules/courses/server/models/course.server.model.js
@@ -4,28 +4,41 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Course Schema
  */
 var CourseSchema = new Schema({
-  courseId:{
-    type:String,
-      default: ''
-  },
-    courseSection:{
-    type: String ,
+    courseId: {
+        type: String,
         default: ''
     },
-  courseTitle: {
-    type: String,
-    default: ''
-  },
-  instructor:{
-    type: String,
-      default:''
-  }
+    courseSection: {
+        type: String,
+        default: ''
+    },
+    courseTitle: {
+        type: String,
+        default: ''
+    },
+    instructor: {
+        type: String,
+        default: ''
+    },
+    enrollmentAddDrop: {
+        type: Number
+    },
+    enrollmentSemester: {
+        type: Number
+    },
+    numberOfSection: {
+        type: Number
+    },
+    taHours: {
+        type: Number
+    }
+
 
 });
 

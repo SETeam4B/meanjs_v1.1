bdf48c01a00ec6911605c1133f4b228e6c5ec0c1
mtellmann <miketellmann@gmail.com>
2017-10-29 20:55:08 -0400

    added inject for course listing

--- modules/facultyhomepages/client/controllers/list-facultyhomepages.client.controller.js
+++ modules/facultyhomepages/client/controllers/list-facultyhomepages.client.controller.js
@@ -5,12 +5,13 @@
     .module('facultyhomepages')
     .controller('FacultyhomepagesListController', FacultyhomepagesListController);
 
-  FacultyhomepagesListController.$inject = ['FacultyhomepagesService', 'Users'];
+  FacultyhomepagesListController.$inject = ['FacultyhomepagesService', 'Users', 'CoursesService'];
 
-  function FacultyhomepagesListController(FacultyhomepagesService, Users) {
+  function FacultyhomepagesListController(FacultyhomepagesService, Users, CoursesService) {
     var vm = this;
     vm.user = Users.query();
-
+    vm.courses = CoursesService.query();
     vm.facultyhomepages = FacultyhomepagesService.query();
+
   }
 }());

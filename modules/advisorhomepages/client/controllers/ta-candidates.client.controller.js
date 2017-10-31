3e48cfe38176b6063e3cfd4c63830e22bfdb3589
jordiahl <jordiahl@ufl.edu>
2017-10-31 09:30:02 -0400

    fixed fake data and ui

--- modules/advisorhomepages/client/controllers/ta-candidates.client.controller.js
+++ modules/advisorhomepages/client/controllers/ta-candidates.client.controller.js
@@ -15,40 +15,45 @@
         $scope.isAdvisorForm = true;
         $scope.TACandidateForms = [{}];
         var fs = new FormsService();
-        $scope.allStudentForms = FormsService.getAll();
 
         $scope.fakeData = [
             {
                 "id": 1,
                 "name": "Lionel Messi",
-                "type": 123,
-                "yolo1": "exra Parameter"
+                "category": 123,
+                "hourTA": "exra Parameter"
             },
             {
                 "id": 2,
                 "name": "Ronaldinho",
-                "type": 456,
-                "yolo1": "exra Parameter"
+                "category": 123,
+                "hourTA": "exra Parameter"
             },
             {
                 "id": 3,
                 "name": "Zinedine Zidan",
-                "type": 789,
-                "yolo1": "exra Parameter"
+                "category": 123,
+                "hourTA": "exra Parameter"
             },
             {
                 "id": 4,
                 "name": "Luis Suarez",
-                "type": 987,
-                "yolo1": "exra Parameter"
+                "category": 123,
+                "hourTA": "exra Parameter"
             },
             {
                 "id": 0,
                 "name": "Maradona",
-                "type": 1,
-                "yolo1": "exra Parameter"
+                "category": 123,
+                "hourTA": "exra Parameter"
             }
         ];
+
+
+        fs.$getAll().then(function (res) {
+            $scope.fakeData = res.data;
+        });
+
 
         $scope.openModal = function () {
             $modal.open({

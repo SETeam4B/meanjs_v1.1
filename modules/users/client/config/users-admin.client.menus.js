9bead526ce73942295d11f5706eed998a676714f
mtellmann <miketellmann@gmail.com>
2017-11-28 02:19:43 -0500

    sorry if it breaks

--- modules/users/client/config/users-admin.client.menus.js
+++ modules/users/client/config/users-admin.client.menus.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Configuring the Articles module
+Configuring the Articles module
 angular.module('users.admin').run(['Menus',
   function (Menus) {
     Menus.addSubMenuItem('topbar', 'admin', {
